94b948f119b299a8c18192734901f4bba60ee4e8
RoaryStar <hanssun07@gmail.com>
2019-02-09 13:18:51 -0500

    refactor files

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 A program that simulates Rock-Paper-Scissors tournaments.
 
 This program is written exclusively in C99.
+
+Compile on command line with ``gcc -std=c99 RPS.c RPS_champion.c -o RPS`` and run with ``./RPS``.
+
---
+++ RPS.h
@@ -0,0 +1,39 @@
+#ifndef RPS.h
+#define RPS.h
+
+double wrkbuffer [1024];
+
+#define ROCK        0
+#define PAPER       1
+#define SCISSORS    2
+
+#define G_PONE      '1'
+#define G_PTWO      '2'
+#define G_TIE       't'
+
+typedef char gres_t;     // one of G_PONE, G_PTWO, G_TIE
+typedef int toss_t;      // one of ROCK, PAPER, or SCISSORS
+
+// winner : toss, toss -> g_res
+// Results the winer in a game where the lhs player
+// throws p1 and the rhs player throws p2.
+gres_t winner (toss_t p1, toss_t p2);
+
+typedef void* pdata_t;
+
+// data encapsulation for a player
+typedef struct
+{
+    // stored data (histories etc.)
+    pdata_t data;
+
+    // choose_f : pdata -> toss
+    toss_t (*choose_f) (pdata_t);
+
+    // learn_f : pdata, toss, toss | updates pdata
+    void (*learn_f) (pdata_t, toss_t, toss_t);
+} player_t;
+
+player_t* make_champion_player (player_t* p, int n)
+
+#endif
